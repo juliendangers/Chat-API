b10391b170f41e4243984c3e93015359ed6807c1
Julien Crestin <me@juliencrestin.com>
2016-07-06 13:53:39 +0200

    fix(LibAxolotl): add missing namespace to test

--- src/libaxolotl-php/tests/util/test_byteutil.php
+++ src/libaxolotl-php/tests/util/test_byteutil.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once __DIR__.'/../../util/ByteUtil.php';
+namespace LibAxolotl\Tests\Utils;
 
-class ByteUtilTest extends PHPUnit_Framework_TestCase
+use LibAxolotl\Utils\ByteUtil;;
+
+class ByteUtilTest extends \PHPUnit_Framework_TestCase
 {
     public function testSplit()
     {
-        $okm = pack('H*', '02a9aa6c7dbd64f9d3aa92f92a277bf54609dadf0b00828acfc61e3c724b84a7bfbe5efb603030526742e3ee89c7024e884e'.
-                 '440f1ff376bb2317b2d64deb7c8322f4c5015d9d895849411ba1d793a827');
-
         $data = '';
         for ($i = 0; $i < 80; $i++) {
             $data .= chr($i);
